4ef64843d7485aea5e8eed629f393a567686344b
unknown <hitesh.visit@gmail.com>
2021-03-25 23:40:50 +0530

    d

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
-// HTTP
-define('HTTP_SERVER', 'http://127.0.0.1/rentopcmain1/admin/');
-define('HTTP_CATALOG', 'http://127.0.0.1/rentopcmain1/');
 
-// HTTPS
-define('HTTPS_SERVER', 'http://127.0.0.1/rentopcmain1/admin/');
-define('HTTPS_CATALOG', 'http://127.0.0.1/rentopcmain1/');
+define('HTTP_SERVER', 'https://www.rentopc.com/admin/');
+define('HTTP_CATALOG', 'https://www.rentopc.com/');
+
+
+define('HTTPS_SERVER', 'https://www.rentopc.com/admin/');
+define('HTTPS_CATALOG', 'https://www.rentopc.com/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/rentopcmain1/admin/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/rentopcmain1/system/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/rentopcmain1/image/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/rentopcmain1/admin/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/rentopcmain1/admin/view/template/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/rentopcmain1/system/config/');
-define('DIR_CACHE', 'C:/xampp/htdocs/rentopcmain1/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/rentopcmain1/system/storage/download/');
-define('DIR_LOGS', 'C:/xampp/htdocs/rentopcmain1/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/rentopcmain1/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/rentopcmain1/system/storage/upload/');
-define('DIR_CATALOG', 'C:/xampp/htdocs/rentopcmain1/catalog/');
+define('DIR_APPLICATION', '/home/admin/web/rentopc.com/public_html/admin/');
+define('DIR_SYSTEM', '/home/admin/web/rentopc.com/public_html/system/');
+define('DIR_IMAGE', '/home/admin/web/rentopc.com/public_html/image/');
+define('DIR_LANGUAGE', '/home/admin/web/rentopc.com/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/admin/web/rentopc.com/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/admin/web/rentopc.com/public_html/system/config/');
+define('DIR_CACHE', '/home/admin/web/rentopc.com/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/admin/web/rentopc.com/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/admin/web/rentopc.com/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/admin/web/rentopc.com/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/admin/web/rentopc.com/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/admin/web/rentopc.com/public_html/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'rentopc');
+define('DB_USERNAME', 'admin_rentopc');
+define('DB_PASSWORD', 'ihI7nXd5iG');
+define('DB_DATABASE', 'admin_rentopc');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
