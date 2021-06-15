494836dbaae877db22057d712929e32852702902
unknown <hitesh.visit@gmail.com>
2021-06-15 10:50:26 +0530

    push

--- system/PHPExcel/Classes/PHPExcel/Shared/File.php
+++ system/PHPExcel/Classes/PHPExcel/Shared/File.php
@@ -130,17 +130,17 @@ class PHPExcel_Shared_File
      */
     public static function sys_get_temp_dir()
     {
-        if (self::$useUploadTempDirectory) {
-            //  use upload-directory when defined to allow running on environments having very restricted
-            //      open_basedir configs
-            if (ini_get('upload_tmp_dir') !== false) {
-                if ($temp = ini_get('upload_tmp_dir')) {
-                    if (file_exists($temp)) {
-                        return realpath($temp);
-                    }
-                }
-            }
-        }
+        // if (self::$useUploadTempDirectory) {
+        //     //  use upload-directory when defined to allow running on environments having very restricted
+        //     //      open_basedir configs
+        //     if (ini_get('upload_tmp_dir') !== false) {
+        //         if ($temp = ini_get('upload_tmp_dir')) {
+        //             if (file_exists($temp)) {
+        //                 return realpath($temp);
+        //             }
+        //         }
+        //     }
+        // }
 
         // sys_get_temp_dir is only available since PHP 5.2.1
         // http://php.net/manual/en/function.sys-get-temp-dir.php#94119
